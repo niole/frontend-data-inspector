4965365090bbfb61438221f8253d8207c6243005
niole <niolenelson@gmail.com>
2023-10-29 10:08:38 -0700

    Integrate daisy UI refactor UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { createEffect, onMount, createSignal } from 'solid-js';
+import { onMount, createSignal } from 'solid-js';
 import { Chart, Title, Tooltip } from 'chart.js'
 import { DefaultChart } from 'solid-chartjs';
 import { groupBy } from 'ramda';
 
 const COLORS = ['#d81919', '#d6b012', '#26a100', '#b9ba0b', '#ff5100', '#ff8851', '#2aa0b7', '#6ccadc', '#a7e8f5'];
-
-const mockData: VisDataPoint[] = [
-  {
-    point:[105.95724, 298.96707],
-    content: "asdf",
-    centroid_index: 1
-  },
-];
 
 type VisDataPoint = {
   point: [number, number],
@@ -20,10 +12,13 @@ type VisDataPoint = {
 };
 
 function App() {
+  const [k, setK] = createSignal<number>(0);
+  const [textSnippets, setTextSnippets] = createSignal<string>("");
   const [color, setContentColor] = createSignal<string | undefined>();
   const [points, setPoints] = createSignal<VisDataPoint[]>([]);
   const [selectedContent, setContent] = createSignal<string | undefined>();
   const [uri, setUri] = createSignal<string | undefined>();
+  const [loadingData, setLoadingData] = createSignal<boolean>(false);
   onMount(() => {
     Chart.register(Tooltip, Title);
   });
@@ -48,19 +43,55 @@ function App() {
       }
   };
 
+  async function handleLoading(p: Promise<void>) {
+    setLoadingData(true);
+    try {
+      await p;
+    } finally {
+      setLoadingData(false);
+
+    }
+
+  }
+
   function getUriData() {
-    fetch(`http://localhost:8080/import?uri=${uri()}&k=5`)
+    handleLoading(fetch(`http://localhost:8080/import?uri=${uri()}&k=${k()}`)
     .then(x => x.json())
     .then(x => {
         setPoints(() => x.data);
-      });
+      }));
+  }
+
+  function getTextSnippetData() {
+    const body = { k: k(), data: JSON.parse(textSnippets()) };
+    handleLoading(fetch('http://localhost:8080/import',
+      { method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(body) }
+    )
+    .then(x => x.json())
+    .then(x => {
+        setPoints(() => x.data);
+      }));
   }
 
   return (
     <>
-      <input placeholder="uri" onChange={e => setUri(e.target.value)}/><button onClick={getUriData}>uri</button>
-      <div style={{ display: 'flex', 'flex-direction': 'row'}}>
+      <div>
+        <div style={{display: 'flex', 'flex-direction': 'row'}}>
+          <input class="input input-bordered w-20 m-2" placeholder="k" onChange={e => setK(parseInt(e.target.value))} type="number"/>
+          <div style={{display: 'flex', 'flex-direction': 'row'}}>
+            <input placeholder="uri" onChange={e => setUri(e.target.value)} class="input input-bordered max-w-xs m-2" />
+            <textarea class="textarea textarea-bordered m-2" placeholder="JSON text snippets" onChange={e => setTextSnippets(e.target.value)}></textarea>
+          </div>
+        </div>
+        <div>
+          <button class="btn btn-primary m-2" onClick={getUriData}>Send URI</button>
+          <button class="btn btn-primary m-2"  onClick={getTextSnippetData}>Send Text Snippets</button>
+          {loadingData() && <span class="loading loading-ball loading-md"></span> }
+        </div>
+      </div>
+      <div style={{ display: 'flex', 'flex-direction': 'row', height: '500px'}}>
         <span>
+          {points().length === 0 && 'No data yet'}
           <DefaultChart
             type="scatter"
             data={{
@@ -82,7 +113,7 @@ function App() {
         </span>
         <div style={{background: color(), 'overflow-y': 'auto', height: '500px'}}>{selectedContent()}</div>
       </div>
-    </>
+      </>
   );
 }
 
